c97d0764316392fa13fcd259d119cdba380c754a
yqfang <404762352@qq.com>
2016-07-04 00:08:42 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('clean', function() {
 })
 gulp.task('scripts', ['clean'], function() {
 	var buildNg = function() {
-		return gulp.src(['ng-pack/angular.js', 'ng-pack/!(ngpack)*.js', 'ng-pack/ngpack.js'])
+		return gulp.src(['ng-pack/jquery.js', 'ng-pack/angular.js', 'ng-pack/!(ngpack)*.js', 'ng-pack/ngpack.js'])
 	}
 	var buildIE = function() {
 		return gulp.src('ie-pack/*.js')
