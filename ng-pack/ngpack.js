28b693ef01c23e03ffdfaaa310b8f48ee85e719d
yqfang <404762352@qq.com>
2016-07-10 20:05:05 +0800

    add checklist

--- ng-pack/ngpack.js
+++ ng-pack/ngpack.js
@@ -1,4 +1,4 @@
-angular.module('ng-package', ['ngSanitize', 'ngAnimate', 'ui.bootstrap', 'ui.select', 'ui.router', 'dialogs.main', 'oc.lazyLoad', 'ng.shims.placeholder', 'ngLocale'])
+angular.module('ng-package', ['ngSanitize', 'ngAnimate', 'ui.bootstrap', 'ui.select', 'ui.router', 'dialogs.main', 'oc.lazyLoad', 'ng.shims.placeholder', 'ngLocale', 'checklist-model'])
 		.config(['datepickerConfig', '$translateProvider', 'dialogsProvider', function(datepickerConfig, $translateProvider, dialogsProvider) {
 			datepickerConfig.showWeeks = false;
 			dialogsProvider.setSize("md");
